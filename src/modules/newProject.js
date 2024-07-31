bd72c8a54e612c484fc1a0df3984599ee10790a6
karldreta <karldreta@gmail.com>
2024-07-31 10:50:57 +0800

    Add newProject module at src/modules

---
+++ src/modules/newProject.js
@@ -0,0 +1,54 @@
+const newProjectBtn = document.querySelector('#newProjectBtn');
+newProjectBtn.addEventListener('click', addNewProject);
+const body = document.querySelector('body');
+const projectInputDialog = document.createElement('dialog');
+
+export default function addNewProject () {
+    projectInputDialog.innerHTML = `
+        <h2>Create New Project</h2>
+        <form action="#" method="post">
+            <label for="projectName">Project Name:</label>
+            <input type="text" id="projectName">
+            <div class="showError" id="nameError"></div>
+        
+            <label for="projectDescription">Description:</label>
+            <input type="text" id="projectDescription">
+            <div class="showError" id="descriptionError"></div>
+        
+            <label for="projectDueDate">Due Date:</label>
+            <input type="datetime-local" id="projectDueDate">
+            <div class="showError" id="dueDateError"></div>
+        
+            <fieldset>
+                <legend>Priority Level:</legend>
+
+                <input type="radio" id="priorityRelaxed" name="projectPriority" value="relaxed">
+                <label for="priorityRelaxed">Relaxed</label>
+
+                <input type="radio" id="priorityUrgent" name="projectPriority" value="urgent">
+                <label for="priorityUrgent">Urgent</label>
+
+                <input type="radio" id="priorityImportant" name="projectPriority" value="important">
+                <label for="priorityImportant">Important</label>
+
+                <div class="showError" id="priorityError"></div>
+            </fieldset>
+            <button type="submit">Submit</button>
+        </form>
+    `;
+    
+    body.appendChild(projectInputDialog);
+    projectInputDialog.showModal();
+}
+
+projectInputDialog.addEventListener("click", e => {
+      const dialogDimensions = projectInputDialog.getBoundingClientRect();
+      if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+      ) {
+        projectInputDialog.close();
+      }
+    });
--- src/script.js
+++ src/script.js
@@ -1,1 +1,2 @@
 import './style.css';
+import addNewProject from './modules/newProject.js';
