969e7bf75670117d74bb1ea70097af2bbab90107
karldreta <karldreta@gmail.com>
2024-08-18 08:25:55 +0800

    Refactor Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/script.js',
-  devtool: 'inline-source-map',
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
         template: './src/template.html',
@@ -32,6 +35,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      // {
+      //   test: /\.html$/i,
+      //   loader: "html-loader",
+      // },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
