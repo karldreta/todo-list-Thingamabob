989078541764c2b75d9e5bc42b185b3026fc9422
karldreta <karldreta@gmail.com>
2024-08-22 05:43:56 +0800

    Initial dist subtree commit

--- src/script.js
+++
@@ -1,7 +0,0 @@
-import './style.css';
-import addNewProject from './modules/projectForm.js';
-import { sortByPriority, sortByDueDate } from './modules/forSorting.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#newProjectBtn').addEventListener('click', addNewProject);
-});
